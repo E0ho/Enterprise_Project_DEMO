02d7054d891d45d492ae20677ae91824dc4fe505
E0ho <bbchoi6@hufs.ac.kr>
2022-10-28 01:05:36 +0900

    규칙2 HTML 파싱

---
+++ Youngho/Rule_Page/Rule2.py
@@ -0,0 +1,66 @@
+import requests
+from bs4 import BeautifulSoup
+import openpyxl
+import time
+
+from email import header
+from http.client import ImproperConnectionState
+import urllib.request
+from urllib.error import URLError, HTTPError
+
+lists = [
+    "http://rimrim.co.kr",
+    # "https://themedicube.co.kr/"
+]
+
+# categoryNum
+cate_no = [24, 25, 466]
+
+# 엑셀 생성
+workbook = openpyxl.Workbook()
+
+
+# 자동적으로 반복
+for list in lists:
+
+    # 엑셀 스타일 시트 생성
+    worksheet = workbook.create_sheet('사이트')
+    print(list)
+
+    # 엑셀 행 지정
+    i = 2
+    worksheet['A1'] = '해당 상품 사이트'
+    worksheet['D1'] = '상품명'
+    worksheet['H1'] = '가격'
+
+    # html 규칙 2 (주소/product/detail.html?product_no=(int)&cate_no=(int)&display_group=(int))
+    for num3 in range(1, 3):
+        for num2 in cate_no:
+            for num1 in range(1, 200):
+
+                # 상품 판매 링크 가져오기
+                header = {'User-Agent': 'Chrome/66.0.3359.181'}
+                response = requests.get(
+                    f"{list}/product/detail.html?product_no={num1}&cate_no={num2}&display_group={num3}", headers=header)
+
+                # 해당 url 존재 유무 파악
+                if response.status_code == 200:
+
+                    # 파싱
+                    html = response.text
+                    soup = BeautifulSoup(html, 'html.parser')
+
+                    # 원하는 정보 추출
+                    name = soup.select_one('.infoArea .name').text
+                    # else:
+                    #     name = soup.select_one(
+                    #         '.infoArea .prd_name_wrap')
+
+                    price = soup.select_one('.infoArea .price').text
+                    print(list, name, price)
+                    worksheet[f'A{i}'] = list
+                    worksheet[f'D{i}'] = name
+                    worksheet[f'H{i}'] = price
+                    i = i+1
+
+workbook.save('Youngho/Rule_Page/rule2.xlsx')

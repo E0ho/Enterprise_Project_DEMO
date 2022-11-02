4266f0bd70cb265e01482e0fa85bfd9796fe684d
Youngsu Na <dudtnsk54@naver.com>
2022-11-02 20:07:42 +0900

    json, html 연동 완료

--- Youngsu/Databaseconnect.py
+++ Youngsu/Databaseconnect.py
@@ -9,7 +9,7 @@ from http.client import ImproperConnectionState
 from urllib.error import URLError, HTTPError
 import string
 import time
-import signal
+import re
 
 
 # STEP 2: MySQL Connection 연결
@@ -19,42 +19,45 @@ con = pymysql.connect(host='127.0.0.1', user='root', password='fouridiot1234',
 # STEP 3: Connection 으로부터 Cursor 생성
 cur = con.cursor()
 
-lists = [
-    "https://m.ycloset.com/"
-    # "https://m.mainbooth.co.kr/"
-    # "http://rimrim.co.kr",
-    # "https://themedicube.co.kr/",
-    # "https://www.andar.co.kr"
-]
+cur.execute("delete from platform_item")
+con.commit()
+# lists = [
+        # "https://m.ycloset.com/"
+#     # "https://m.mainbooth.co.kr/"
+#     # "http://rimrim.co.kr",
+#     # "https://themedicube.co.kr/",
+#     # "https://www.andar.co.kr"
+# ]
+
+arr2 = []
+num2 = cur.execute("SELECT * FROM html_url")
+con.commit()
+result = cur.fetchall()
+for record in result:
+    arr2.append(record)
+    print(arr2[0][1])
 
 # func1이 실행되면 cafe의 json을 가진 사이트를 파싱
-def func1(list, k, app_key) : 
-        # print(requestdata.status_code)
-        # 실제 있는 사이트인지 확인하는 과정이 있어야 할 것 같다.
-        # timeout전에 되면 json이 있는 것이고 없으면 html로 넘어가게 한다.
-        
-    try :
-        requestdata = requests.get(f"https://{list}.cafe24api.com/api/v2/products/{k}?cafe24_app_key={app_key}")
-        if requestdata.status_code==200 :
-            print('fun1')
-            req = requestdata.json()
-            return req
-    
-    except :
-        #delay를 안주게 되면 오류가 발생(대량의 request때문에 발생하는 것 같다.)
+def jsonParsing(list, k, app_key) : 
+    try:
+        requestdata = requests.get(f"https://{list}.cafe24api.com/api/v2/products/{k}?shop_no=1&cafe24_app_key={app_key}")
+        return requestdata
+    except:
         time.sleep(1)
-        return func2()
+        return htmlParsing()
+    
    
 # 모든 function이 실행되지 않으면 어쩔 수 없이 html 전체를 파싱해서 값을 가져오기(ex 메디큐브)
-def func2() :
-    print('fun2')
+def htmlParsing() :
     # list를 일단 1개 받아서 num1으로 main에서 돌리기
-    for hlist in lists:
-        for k in range(5300, 5500):
+    for i in range(num2):
+        for k in range(180, 205):
         # 상품 판매 링크 가져오기
             header = {'User-Agent': 'Chrome/66.0.3359.181'}
+            url_name = arr2[0][i]
             requestdata = requests.get(
-                f"{hlist}/product/detail.html?product_no={k}", headers=header)
+                f"{url_name}/product/detail.html?product_no={k}", headers=header)
+
             # 해당 url 존재 유무 파악
             if requestdata.status_code == 200:
                 # 파싱
@@ -82,12 +85,15 @@ def func2() :
                 if price_tag != None and name_tag != None:
                     name = name_tag.text
                     price = price_tag.text
+                    price = int(re.sub(r'[^0-9]', '', price))
+                    print(price)
 
                     print('product_name :' + name, price)
 
-                return name, price
+                ##정규화해서 platform이름 넣어주기(hlist)
+                cur.execute(f"INSERT INTO platform_item VALUES('{name}','{price}','{arr2[0][i]}')")
+                con.commit()
 
-    
 
 # python은 인터프리터 언어이기 때문에 순서를 잘 생각해야 한다.
 
@@ -106,48 +112,47 @@ def func2() :
 
 #list형태로 DB에서 받아와서 돌려보자
 ##두개 짝을 지어서 밑의 for문에 넣어줘야 할 듯
-lists1 = cur.execute("SELECT platform_name FROM platform_input")
+arr1 = []
+num1 = cur.execute("SELECT * FROM platform_input")
 con.commit()
-print(cur.fetchall())
-##결과 (('mall66',), ('ozkiz1',))
-lists2 = cur.execute("SELECT key_api FROM platform_input")
-con.commit()
-print(cur.fetchall())
-##결과 (('f7kOrfNK8UAn2Z93owrB4C',), ('KU5HdZg4BVXlfoLDEPu6EC',))     
+result = cur.fetchall()
+for record in result:
+    print(record)
+    arr1.append(record)
+    print(arr1[0][1])
+##db에 있는 만큼 arr에 넣기 위해서 필요
 
-for platform in lists2:
-    for k in range(5300, 5500) :
-
+#json parsing part
+for i in range(num1) :
+    for k in range(3000, 3011) :
         #위의 for문에 따라 parameter를 다르게 줘야함.
-        requestdata = func1(platform, k, key)
-
+        platform = arr1[i][0]
+        key = arr1[i][1]
+        requestdata = jsonParsing(platform, k, key)
         # type으로 구분하는 걸로 바꿔야 할듯(json은 dict형태로 들어오기 때문에)
-        if type(requestdata)== dict :
+        # if type(requestdata)== dict :
         
-            #jsonData = requestdata.json()
-            for data in requestdata :
-                
-                if requestdata.get(data).get("price") != None :
+        if requestdata.status_code == 200 :
+            jsonData = requestdata.json()
+            for data in jsonData :
+                if jsonData.get(data).get("price") != None :
 
-                    price :int = requestdata.get(data).get("price")
-                    code :int = requestdata.get(data).get("product_code")
-                    tax_free_price :int = requestdata.get(data).get("price_excluding_tax")
-                    name :string = requestdata.get(data).get("product_name")
-                    platform_name :string = platform
-
-                    str = f"INSERT IGNORE INTO platform_item VALUES('{name}', '{code}' ,'{price}','{tax_free_price}','{platform_name}')"
-
+                    price :int = jsonData.get(data).get("price")
+                    # code :int = jsonData.get(data).get("product_code")
+                    # tax_free_price :int = jsonData.get(data).get("price_excluding_tax")
+                    name :string = jsonData.get(data).get("product_name")
+                    platform_name :string = arr1[i][0]
+                    
+                    str = f"INSERT IGNORE INTO platform_item VALUES('{name}','{price}','{platform_name}')"
                     cur.execute(str)
-
                     con.commit()
-
-                    print(data, " : ", price, ", product_code", " : ", code)
-                
+                    print(data, " : ", price, ", platform_name"," : ", platform_name)
                 else :
                     continue
+        else:
+            continue
 
-        #근데 이렇게 만들면 func2가 너무 여러번 돌아서 값이 이상하게 나옴...
-        #func2 내부에서도 for문으로 돌고 위에서도 for문으로 돌아서 이상함.
-        else:      
-            func2()
+#html parsing part
+htmlParsing()
+
 con.close()
--- Youngsu/Savinginput.py
+++ Youngsu/Savinginput.py
@@ -8,12 +8,19 @@ con = pymysql.connect(host='127.0.0.1', user='root', password='fouridiot1234',
 # STEP 3: Connection 으로부터 Cursor 생성
 cur = con.cursor()
 
-key = pyautogui.prompt("key 값을 입력해주세요..")
-platform = pyautogui.prompt("json header를 입력해주세요..")
-
-str2= f"INSERT INTO platform_input VALUES('{platform}', '{key}')"
-cur.execute(str2)
-con.commit()
+key = pyautogui.prompt("key 값을 입력해주세요..(없다면 None을 입력해주세요)")
+if key == "None" :
+        url = pyautogui.prompt("url을 입력해주세요.")
+        
+        ##정규화해서 platform이름을 가져오기
+        cur.execute(f"INSERT INTO html_url VALUES('{url}','{url}')")
+        con.commit()
+else:
+        header = pyautogui.prompt("json header를 입력해주세요..")
+        str2= f"INSERT INTO platform_input VALUES('{header}', '{key}')"
+        cur.execute(str2)
+        con.commit()
+con.close()
 
 
 #만약 하나씩 저장할 때는 아래와 같이 코드 작성

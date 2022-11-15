ea8a9495f78cc3859349b61474b4c1f5cc6babe9
hoyoung2 <pon07060@gmail.com>
2022-11-15 14:07:41 +0900

    class modulation update #2

--- hoyoung/JsonClass.py
+++ hoyoung/JsonClass.py
@@ -2,36 +2,58 @@ import HTMLClass as html
 
 class ParsingJson:
 
+    import pymysql
     import requests
     import json
     from bs4 import BeautifulSoup
 
-    def isAbleJson(self, shop_api_name, app_key): 
-        for k in range(129944, 129977):
-            requestData = ParsingJson.requests.get(f"https://{shop_api_name}.cafe24api.com/api/v2/products/{k}?shop_no=1&cafe24_app_key={app_key}")
-            
-            if requestData.status_code == 200:
-                x = html.ParsingHTML()
-                print(x.option_parsing(k))
-                ParsingJson.jsonParsing(requestData)
+    json_platform_list = []
+    return_value = []
+    wanted_value = {
+
+        "product" : "price",
+        "product" : "product_name"
+        
+    }
+
+    def connectDB():        
+        con = ParsingJson.pymysql.connect(host='127.0.0.1', user='root', password='fouridiot1234',
+                       db='Capstone', charset='utf8') # 한글처리 (charset = 'utf8')
+
+        # STEP 3: Connection 으로부터 Cursor 생성
+        cur = con.cursor()
+
+        cur.execute("delete from platform_item")
+        con.commit()
+
+        count_json_list = cur.execute("SELECT * FROM platform_input")        
+        con.commit()
+        ParsingJson.json_platform_list = cur.fetchall()
+        isAbleJson(count_json_list)
+
+
+    def isAbleJson(self, count_json_list):
+
+        for i in range(count_json_list) :
+            platform = ParsingJson.json_platform_list[i][0]
+            key = ParsingJson.json_platform_list[i][1]
+
+            for k in range(129944, 129977):
+                requestData = ParsingJson.requests.get(f"https://{platform}.cafe24api.com/api/v2/products/{k}?shop_no=1&cafe24_app_key={key}")
+                
+                if requestData.status_code == 200:
+                    x = html.ParsingHTML()
+                    ParsingJson.return_value.append(x.option(k))
+                    ParsingJson.jsonParsing(requestData)
    
     def jsonParsing(requestData):         
         
         jsonData = requestData.json()
-        # print(jsonData)
 
-        json_html_list = ['description']
-        print('=' * 170)
-        for i in jsonData['product'].keys():
-            if i in json_html_list:
-                k = jsonData['product'].get(i)
-                #print(k)
-
-                
-                # 사이트내 여러 선택사항
-                #print(frame)
+        for i in ParsingJson.wanted_value.values():
+            ParsingJson.return_value.append(jsonData['product'][i])
                 
                 
 if __name__ == "__main__":
     p = ParsingJson()
-    p.isAbleJson("mall66", "f7kOrfNK8UAn2Z93owrB4C")
+    p.connectDB()
